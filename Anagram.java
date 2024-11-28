82f0d113c1c3f5cc88771cdfafa8f2a8753f88ea
lieluzan3 <lieluzan3@gmail.com>
2024-11-29 00:43:41 +0200

    submission3

--- Anagram.java
+++ Anagram.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
-/** Functions for checking if a given string is an anagram. */
+
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
@@ -12,16 +10,16 @@ public class Anagram {
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
+		
 		System.out.println(preProcess("What? No way!!!"));
 		
-		// Tests the randomAnagram function.
+		
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
-		// Performs a stress test of randomAnagram 
+		
 		String str = "1234567";
 		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
+
 		for (int i = 0; i < 10; i++) {
 			String randomAnagram = randomAnagram(str);
 			System.out.println(randomAnagram);
@@ -31,56 +29,52 @@ public class Anagram {
 		System.out.println(pass ? "test passed" : "test Failed");
 	}  
 
-	
 	public static boolean isAnagram(String str1, String str2) {
-  //מקבל את מחרוזות ומעדכן אותם במשתנה בפעולה
-    str1 = preProcess(str1);
-    str2 = preProcess(str2);
-    
-    
-    if (str1.length() != str2.length()) {
-        return false;
-    }
-    
-    // מיון האותיות בשני המחרוזות והשוואה ביניהם
-    char[] arr1 = str1.toCharArray();
-    char[] arr2 = str2.toCharArray();
-
-    Arrays.sort(arr1);
-    Arrays.sort(arr2);
-    
-    return Arrays.equals(arr1, arr2);		
+		// מנקים את המחרוזות
+		String cleanStr1 = preProcess(str1);
+		String cleanStr2 = preProcess(str2);
+		
+		
+		if (cleanStr1.length() != cleanStr2.length()) {
+			return false;
+		}
+		
+		// ממירים למערך, ממיינים ומשווים
+		char[] arr1 = cleanStr1.toCharArray();
+		char[] arr2 = cleanStr2.toCharArray();
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		return Arrays.equals(arr1, arr2);
 	}
 	   
 	
 	public static String preProcess(String str) {
-		str = str.replaceAll("\\s", ""); 
-		str = str.replaceAll("[^a-zA-Z]", "");
-		str = str.toLowerCase();         
-		return str;
-	
-	} 
+		if (str == null) {
+			return "";
+		}
+		
+		return str.replaceAll("[^a-zA-Z]", "").toLowerCase();
+	}
 	   
 	
 	public static String randomAnagram(String str) {
-		str = preProcess(str);  // נשתמש ב-preProcess כדי לנקות את המילה
-    
-    // המרת המילה למערך תוויים
-    List<Character> characters = new ArrayList<>();
-    for (char c : str.toCharArray()) {
-        characters.add(c);
-    }
-    
-    // ערבוב אקראי של התוויים
-    Collections.shuffle(characters);
-    
-    // בניית המילה מחדש מהתוויים המעורבבים
-    StringBuilder sb = new StringBuilder();
-    for (char c : characters) {
-        sb.append(c);
-    }
-    
-    return sb.toString();
 		
-	}
+    String cleanStr = preProcess(str);
+    char[] arr = cleanStr.toCharArray();
+    Random random = new Random();
+    
+    
+    for (int i = arr.length - 1; i > 0; i--) {
+        int j = random.nextInt(i + 1);
+        // החלפת מקומות
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    return new String(arr);
 }
+
+	}
+
